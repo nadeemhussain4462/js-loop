a1c834761d30ea6944a82325810fe89d871b505a
Nadeem Hussain <hoteleyenadeem@gmail.com>
2024-09-15 10:59:45 +0500

    assignment 6

---
+++ app.js
@@ -0,0 +1,21 @@
+alert("hello world");
+
+var cleancity = ["karachi", "islamabad", "lahore", "faisalabad"];
+
+if (citycheck === cleancity[0]) {
+  alert("it is clean city");
+} else if (citycheck === cleancity[1]) {
+  alert("it is clean city");
+} else if (citycheck === cleancity[2]) {
+  alert("it is clean city");
+} else if (citycheck === cleancity[3]) {
+  alert("it is clean city");
+} else {
+  alert("it is not clean city");
+}
+
+for (i = 0; i <= 4; i++) {
+  if (citycheck === cleancity[i]) {
+    alert("it is clean city");
+  }
+}
